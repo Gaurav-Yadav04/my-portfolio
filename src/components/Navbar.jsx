4c5185f4b1f081e7a35bd4d70d9c60ed0cec248b
Gaurav-Yadav04 <gaurav4004ayd@gmail.com.>
2025-10-31 01:21:11 +0530

    add animation in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
 
-const Navbar = () => {
-  return (
-    <div >
-      Navbar
+import { useState } from 'react';
+import OverlyMenu from './OverlyMenu';
+import logo from "../assets/Logo.png"
+
+
+
+export default function Navbar() {
+
+  const [menuOpen , setMenuOpen] = useState(false)
+  const [visible, setVisible] = useState(true)
+
+
+
+return(
+  <>
+  <nav className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 py-4 z-50 transition-transform duration-300 ${visible ? "translate-y-0" : "-translate-y-full"}`}>
+    <div>
+      <img src={logo} alt="logo"className='w-8 h-8' />
+      <div>Gaurav</div>
     </div>
-  )
-}
 
-export default Navbar
+
+  </nav>
+
+  <OverlyMenu />
+  </>
+)
+
+}
--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const contact = () => {
   return (
-    <div className='w-full h-screen'>
-      Contact
+    <div>
+      
     </div>
   )
 }
